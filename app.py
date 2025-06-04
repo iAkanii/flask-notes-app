6081b2c93952920e15f0a1b60dc15fe8a25aa1de
iAkanii <andrei.pastushenko.77@gmail.com>
2025-06-04 09:54:00 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db = SQLAlchemy(app)
+
+# Database
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+
+# Main page
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_note = Note(title=title, content=content)
+        db.session.add(new_note)
+        db.session.commit()
+        return redirect('/')
+
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
